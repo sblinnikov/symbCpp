5c3dba18902a4713dc2c6f3d4c5932fca45fa445
Sergei Blinnikov at Len-Y700-14ISK <Sergei.Blinnikov@itep.ru>
2021-09-08 13:59:54 +0300

    examples/wavepacket.cpp corrected from the full electronic book

--- examples/wavepacket.cpp
+++ examples/wavepacket.cpp
@@ -1,5 +1,4 @@
 // wavepacket.cpp
-
 #include <iostream>
 #include "symbolicc++.h"
 using namespace std;
@@ -10,12 +9,12 @@ Symbolic xt ("xt"), tt ("tt"), B("B");
 Symbolic f1 = B/(sqrt(1+i*tt));
 Symbolic f2 = exp(-(xt*xt)/(2*(1+i*tt)));
 Symbolic psit = f1*f2;
-Symbolic res1 = (psit,xt,2)/2; 
+Symbolic res1 = df(psit,xt,2)/2;
 res1 = res1/f2;
-Symbolic res2 = i*df(psit,tt); 
+Symbolic res2 = i*df(psit,tt);
 res2 = res2/f2;
 Symbolic result1 = res1 + res2;
-cout << "result1 = " << result1 << endl<< endl;
+cout << "result1 = " << result1 << endl << endl;
 Symbolic f1s = f1[i==-i];
 Symbolic f2s = f2[i==-i];
 Symbolic psist = f1s*f2s;
@@ -24,6 +23,6 @@ res3 = res3/f2s;
 Symbolic res4 = -i*df(psist,tt);
 res4 = res4/f2s;
 Symbolic result2 = res3 + res4;
-cout << "result2 = " << result2 << endl <<endl;
+cout << "result2 = " << result2 << endl << endl;
 return 0;
 }
