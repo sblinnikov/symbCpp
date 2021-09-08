32c8b78f9218506a5886c1465f8467fa23a556ef
Sergei Blinnikov at Len-Y700-14ISK <Sergei.Blinnikov@itep.ru>
2021-09-08 21:23:13 +0300

    examples/wkb.cpp added

---
+++ examples/wkb.cpp
@@ -0,0 +1,32 @@
+// wkb.cpp
+#include <iostream>
+#include "symbolicc++.h"
+using namespace std;
+
+int main(void)
+{
+using SymbolicConstant::i;
+Symbolic S("S"), hb("hb"), A("A"), V("V"), EN("EN");
+Symbolic x("x"), m("m");
+
+S = S[x]; A = A[x]; V = V[x];
+Symbolic u = exp(i*(S-i*hb*ln(A))/hb); // equation (2)
+
+// eigenvalue equation (1)
+Symbolic R1 = df(u,x,2) + (2*m)*(EN-V)*u;
+cout << "R1 = " << R1 << endl
+
+// find equations (3)
+Symbolic R2 = R1/u;
+cout << "R2 = " << R2 << endl;
+cout << (R2.coeff(i,1)==0) << endl;
+cout << (R2.coeff(i,0)==0) << endl;
+return 0;
+}
+
+// The output is
+// R1 = i*df(S[x],x,x)*hb^(-1)*e^(i*S[x]*hb^(-1)+ln(A[x]))-df(S[x],x)^(2)*hb^(-2)*e^(i*S[x]*hb^(-1)+ln(A[x]))
+// +2*i*df(S[x],x)*hb^(-1)*df(A[x],x)*A[x]^(-1)*e^(i*S[x]*hb^(-1)+ln(A[x]))+df(A[x],x,x)*A[x]^(-1)*e^(i*S[x]*hb^(-1)+ln(A[x]))+2*m*EN*e^(i*S[x]*hb^(-1)+ln(A[x]))-2*m*V[x]*e^(i*S[x]*hb^(-1)+ln(A[x]))
+// R2 = i*df(S[x],x,x)*hb^(-1)-df(S[x],x)^(2)*hb^(-2)+2*i*df(S[x],x)*hb^(-1)*df(A[x],x)*A[x]^(-1)+df(A[x],x,x)*A[x]^(-1)+2*m*EN-2*m*V[x]
+// df(S[x],x,x)*hb^(-1)+2*df(S[x],x)*hb^(-1)*df(A[x],x)*A[x]^(-1)==0
+// -df(S[x],x)^(2)*hb^(-2)+df(A[x],x,x)*A[x]^(-1)+2*m*EN-2*m*V[x]==0
