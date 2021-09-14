b2e5fa4d2180a2cd26fef33eefded969f601efa8
Sergei Blinnikov at Len-Y700-14ISK <Sergei.Blinnikov@itep.ru>
2021-09-14 23:33:35 +0300

    examples/loperators.cpp corr., output added

--- examples/loperators.cpp
+++ examples/loperators.cpp
@@ -1,14 +1,20 @@
 // loperators.cpp
+// install symbolicc as described in ../README for libsymbcgf
+// compile with (if dir inclide is above):
+// g++ -I ../include -L ~/lib -o loperators.exe loperators.cpp -fno-elide-constructors -lsymbcgf
+// result saved in
+// loperators.out
 
 #include <iostream>
 #include "symbolicc++.h"
 using namespace std;
+
 int main(void)
 {
 using SymbolicConstant::i;
 Symbolic x("x",3), hb("hb"), u("u");
 u = u[x(0),x(1),x(2)];
-Symbolic LX = -i*hb*(x(1)*df(u,x(2))-x(2)*df (u,x(1)));
+Symbolic LX = -i*hb*(x(1)*df(u,x(2))-x(2)*df(u,x(1)));
 Symbolic LY = -i*hb*(x(2)*df(u,x(0))-x(0)*df(u,x(2)));
 Symbolic LZ = -i*hb*(x(0)*df(u,x(1))-x(1)*df(u,x(0)));
 
@@ -23,3 +29,21 @@ cout << "L2 = "<< L2 << endl;
 cout << "RES =" << L2[u==LX]-LX[u==L2] << endl;
 return 0;
 }
+
+// The output is
+//
+// The commutator is
+// -hb^(2)*x1*df(u[x0,x1,x2],x0)+hb^(2)*x0*df(u[x0,x1,x2],x1)
+// L2 = -hb^(2)*x1^(2)*df(u[x0,x1,x2],x2,x2)
+// +2*hb^(2)*x1*df(u[x0,x1,x2],x1)
+// +2*hb^(2)*x1*x2*df(u[x0,x1,x2],x1,x2)
+// +2*hb^(2)*x2*df(u[x0,x1,x2],x2)
+// -hb^(2)*x2^(2)*df(u[x0,x1,x2],x1,x1)
+// -hb^(2)*x2^(2)*df(u[x0,x1,x2],x0,x0)
+// +2*hb^(2)*x2*x0*df(u[x0,x1,x2],x2,x0)
+// +2*hb^(2)*x0*df(u[x0,x1,x2],x0)
+// -hb^(2)*x0^(2)*df(u[x0,x1,x2],x2,x2)
+// -hb^(2)*x0^(2)*df(u[x0,x1,x2],x1,x1)
+// +2*hb^(2)*x0*x1*df(u[x0,x1,x2],x0,x1)
+// -hb^(2)*x1^(2)*df(u[x0,x1,x2],x0,x0)
+// RES = 0
